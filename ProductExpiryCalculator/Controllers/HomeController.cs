8c7cb541be65737401d82c6f424256c3c830fec7
Tom Beech <tbeech@beyondtrust.com>
2022-11-14 12:54:51 +0000

    Add project files.

---
+++ ProductExpiryCalculator/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProductExpiryCalculator.Models;
+using System.Diagnostics;
+
+namespace ProductExpiryCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var productText = System.IO.File.ReadAllText("config/products.json");
+            return View(JsonConvert.DeserializeObject<List<Product>>(productText));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProductExpiryCalculator/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductExpiryCalculator.Models
+{
+    public class Product
+    {
+        public string ProductName;
+        public int DaysUntilExpiry;
+        public DateTime DateOfExpiry => DateTime.Now.AddDays(DaysUntilExpiry);
+
+        public Product(string name, int daysUntilExpiry)
+        {
+            ProductName = name;
+            DaysUntilExpiry = daysUntilExpiry;
+        }
+    }
+}
